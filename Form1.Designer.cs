5b74c0b4b059900e4f5c76bef9b451e0a3fd27b4
Adonis <adrianspl@o2.pl>
2025-04-14 19:01:07 +0200

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,103 +38,170 @@
             button8 = new Button();
             button9 = new Button();
             button10 = new Button();
+            plus = new Button();
+            minus = new Button();
+            divide = new Button();
+            multiply = new Button();
+            window = new TextBox();
             SuspendLayout();
             // 
             // button3
             // 
-            button3.Location = new Point(47, 160);
+            button3.Location = new Point(12, 136);
             button3.Name = "button3";
             button3.Size = new Size(50, 29);
             button3.TabIndex = 2;
             button3.Text = "1";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(103, 160);
+            button1.Location = new Point(68, 136);
             button1.Name = "button1";
             button1.Size = new Size(50, 29);
             button1.TabIndex = 3;
             button1.Text = "2";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(159, 160);
+            button2.Location = new Point(124, 136);
             button2.Name = "button2";
             button2.Size = new Size(50, 29);
             button2.TabIndex = 4;
             button2.Text = "3";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(159, 125);
+            button4.Location = new Point(124, 101);
             button4.Name = "button4";
             button4.Size = new Size(50, 29);
             button4.TabIndex = 7;
             button4.Text = "6";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
-            button5.Location = new Point(103, 125);
+            button5.Location = new Point(68, 101);
             button5.Name = "button5";
             button5.Size = new Size(50, 29);
             button5.TabIndex = 6;
             button5.Text = "5";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // button6
             // 
-            button6.Location = new Point(47, 125);
+            button6.Location = new Point(12, 101);
             button6.Name = "button6";
             button6.Size = new Size(50, 29);
             button6.TabIndex = 5;
             button6.Text = "4";
             button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // button7
             // 
-            button7.Location = new Point(159, 90);
+            button7.Location = new Point(124, 66);
             button7.Name = "button7";
             button7.Size = new Size(50, 29);
             button7.TabIndex = 10;
             button7.Text = "9";
             button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
             // 
             // button8
             // 
-            button8.Location = new Point(103, 90);
+            button8.Location = new Point(68, 66);
             button8.Name = "button8";
             button8.Size = new Size(50, 29);
             button8.TabIndex = 9;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
             // 
             // button9
             // 
-            button9.Location = new Point(47, 90);
+            button9.Location = new Point(12, 66);
             button9.Name = "button9";
             button9.Size = new Size(50, 29);
             button9.TabIndex = 8;
             button9.Text = "7";
             button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
             // 
             // button10
             // 
-            button10.Location = new Point(103, 195);
+            button10.Location = new Point(68, 171);
             button10.Name = "button10";
             button10.Size = new Size(50, 29);
             button10.TabIndex = 11;
             button10.Text = "0";
             button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // plus
+            // 
+            plus.Location = new Point(180, 66);
+            plus.Name = "plus";
+            plus.Size = new Size(50, 29);
+            plus.TabIndex = 12;
+            plus.Text = "+";
+            plus.UseVisualStyleBackColor = true;
+            plus.Click += plus_Click;
+            // 
+            // minus
+            // 
+            minus.Location = new Point(180, 101);
+            minus.Name = "minus";
+            minus.Size = new Size(50, 29);
+            minus.TabIndex = 13;
+            minus.Text = "-";
+            minus.UseVisualStyleBackColor = true;
+            minus.Click += minus_Click;
+            // 
+            // divide
+            // 
+            divide.Location = new Point(180, 136);
+            divide.Name = "divide";
+            divide.Size = new Size(50, 29);
+            divide.TabIndex = 14;
+            divide.Text = "/";
+            divide.UseVisualStyleBackColor = true;
+            divide.Click += divide_Click;
+            // 
+            // multiply
+            // 
+            multiply.Location = new Point(180, 171);
+            multiply.Name = "multiply";
+            multiply.Size = new Size(50, 29);
+            multiply.TabIndex = 15;
+            multiply.Text = "*";
+            multiply.UseVisualStyleBackColor = true;
+            multiply.Click += multiply_Click;
+            // 
+            // window
+            // 
+            window.Location = new Point(12, 33);
+            window.Name = "window";
+            window.Size = new Size(218, 27);
+            window.TabIndex = 16;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(240, 248);
+            Controls.Add(window);
+            Controls.Add(multiply);
+            Controls.Add(divide);
+            Controls.Add(minus);
+            Controls.Add(plus);
             Controls.Add(button10);
             Controls.Add(button7);
             Controls.Add(button8);
@@ -148,6 +215,7 @@
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -161,5 +229,10 @@
         private Button button8;
         private Button button9;
         private Button button10;
+        private Button plus;
+        private Button minus;
+        private Button divide;
+        private Button multiply;
+        private TextBox window;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,11 +2,131 @@ namespace kalkulatorProsty
 {
     public partial class Form1 : Form
     {
+        string value = ""; // Aktualna wartoœæ wprowadzana przez u¿ytkownika
+        double result = 0; // Wynik operacji
+        string operation = ""; // Aktualna operacja (+, -, *, /)
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        
+        private void plus_Click(object sender, EventArgs e)
+        {
+            PerformOperation();
+            operation = "+";
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            PerformOperation();
+            operation = "-";
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            PerformOperation();
+            operation = "/";
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            PerformOperation();
+            operation = "*";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AppendValue(button3.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AppendValue(button1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AppendValue(button2.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            AppendValue(button6.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AppendValue(button5.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AppendValue(button4.Text);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            AppendValue(button9.Text);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            AppendValue(button8.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            AppendValue(button7.Text);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // Przycisk "=" - oblicz wynik
+            PerformOperation();
+            operation = "";
+            window.Text = result.ToString();
+            value = ""; // Resetuj wartoœæ
+        }
+
+        private void AppendValue(string text)
+        {
+            value += text;
+            window.Text = value;
+        }
+
+        private void PerformOperation()
+        {
+            if (double.TryParse(value, out double number))
+            {
+                if (operation == "+")
+                    result += number;
+                else if (operation == "-")
+                    result -= number;
+                else if (operation == "*")
+                    result *= number;
+                else if (operation == "/")
+                {
+                    if (number != 0)
+                        result /= number;
+                    else
+                    {
+                        window.Text = "B³¹d: Dzielenie przez 0";
+                        value = "";
+                        return;
+                    }
+                }
+                else
+                {
+                    result = number; // Pierwsza liczba
+                }
+            }
+
+            // Wyœwietl wynik w polu tekstowym
+            window.Text = result.ToString();
+
+            // Resetuj wprowadzon¹ wartoœæ
+            value = "";
+        }
     }
 }
